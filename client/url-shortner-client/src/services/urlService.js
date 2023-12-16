6d332e3a8b0326991c3a395f1be3ded5afb8a210
mprabesh <theprabesh723@gmail.com>
2023-12-17 00:32:35 +0545

    feat: refactored code for deployment

--- client/url-shortner-client/src/services/urlService.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const baseURL = "/prabeshURL/";
-
-export const processURL = async (originalURL) => {
-  const response = await axios.post(`${baseURL}shorten`, { originalURL });
-  return response.data;
-};
